396141857df8c0370d9b3f0221fb146df86cef98
Nick Stewart <nicktstewart@gmail.com>
2024-05-05 01:41:39 -0400

    [Update] favicon name change

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Gurii Eco Games",
   description: "Enjoy the games you love.",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -17,13 +22,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="container mx-auto">
-          <Header />
-          {children}
-          <Footer />
-        </div>
+    <html lang="en" className={inter.className}>
+      <body>
+        <Header />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
