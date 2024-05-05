cecacea912541ef34752686e3f2902f53c269567
Nick Stewart <nicktstewart@gmail.com>
2024-05-05 03:29:12 -0400

    [Add] BAAS WaywardBones game pages

---
+++ app/games/baas/page.tsx
@@ -0,0 +1,18 @@
+import { Avatar } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold mb-8">
+        Battle Against the Acolytes of Science
+      </h1>
+      <div className="aspect-w-16 aspect-h-9 h-screen">
+        <iframe
+          src="https://guriiecogames.github.io/geg-builds/BAASdemo/"
+          className="w-full h-full"
+        ></iframe>
+      </div>
+    </div>
+  );
+}
---
+++ app/games/wayward_bones/page.tsx
@@ -0,0 +1,16 @@
+import { Avatar } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold mb-8">Wayward Bones</h1>
+      <div className="aspect-w-16 aspect-h-9 h-screen">
+        <iframe
+          src="https://guriiecogames.github.io/geg-builds/WaywardBones/"
+          className="w-full h-full"
+        ></iframe>
+      </div>
+    </div>
+  );
+}
