c31b0f3fc92de3cdfb78806c4b435ce033f99bd1
Takahiro Ogawa <nicktstewart@gmail.com>
2025-03-01 01:35:11 -0500

    [Add] Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -48,6 +49,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="container mx-auto">
+          <Analytics />
           <Header />
           {children}
           <Footer />
